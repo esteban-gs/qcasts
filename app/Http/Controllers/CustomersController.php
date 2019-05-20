5dc3a3a908265ce58cbe93ccd4968a3ba1e42979
Esteban <26440947+esteban-gs@users.noreply.github.com>
2019-05-19 22:37:14 -0700

    vid9

--- app/Http/Controllers/CustomersController.php
+++ app/Http/Controllers/CustomersController.php
@@ -19,8 +19,13 @@ class CustomersController extends Controller
 
     public function store()
     {
+        $data = request()->validate([
+            'name' => 'required|min:3',
+            'email' => 'required|min:5|email'
+        ]);
         $customer = new Customer();
         $customer->name = request('name');
+        $customer->email = request('email');
         $customer->save();
 
         return back();
--- resources/views/internals/customers.blade.php
+++ resources/views/internals/customers.blade.php
@@ -6,7 +6,11 @@
     <form action="customers" method="POST" class="pb-5">
         <div class="form-group">
             <input type="text" name="name">
+            <input type="text" name="email">
         </div>
+
+        <div>{{ $errors->first('name') }}</div>
+        <div>{{ $errors->first('email') }}</div>
 
         <button type="submit">Add Customer</button>
         @csrf
@@ -14,7 +18,9 @@
 
     <ul>
         @foreach ($customers as $customer)
-        <li>{{ $customer->name }} </li>
+        <li>{{ $customer->name }}, {{ $customer->email }} </li>
         @endforeach
     </ul>
 @endsection
+
+
