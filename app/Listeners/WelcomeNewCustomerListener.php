7d3aa1201eb1215acefe7154570803f66b42602c
Esteban <26440947+esteban-gs@users.noreply.github.com>
2019-06-26 19:44:13 -0700

    Queues: Database Driver

--- app/Listeners/WelcomeNewCustomerListener.php
+++ app/Listeners/WelcomeNewCustomerListener.php
@@ -5,8 +5,9 @@ namespace App\Listeners;
 use Illuminate\Queue\InteractsWithQueue;
 use App\Mail\WelcomeNewUserMail;
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Contracts\Queue\ShouldQueue;
 
-class WelcomeNewCustomerListener
+class WelcomeNewCustomerListener implements ShouldQueue
 {
     /**
      * Handle the event.
@@ -17,6 +18,8 @@ class WelcomeNewCustomerListener
     public function handle($event)
     {
         //
+        sleep(10);
+        
         Mail::to($event->customer->email)->send(new WelcomeNewUserMail());
     }
 }
